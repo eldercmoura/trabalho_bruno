f8fe22c809c8495991ecbee5061314fdbb655882
eldercmoura <elder.moura@aluno.faculdadeimpacta.com.br>
2024-11-22 13:06:09 -0300

    Update app_viz.py
    Adicionado a importação do matplotlib como plt

--- 4_scripts/app_viz.py
+++ 4_scripts/app_viz.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import plotly.express as px
+import matplotlib.pyplot as plt
 import numpy as np
 
 st.write("**DADOS DA EMPRESA ESCOLHIDA**")
